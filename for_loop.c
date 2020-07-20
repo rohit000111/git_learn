58286a11d625ccc244f577894b3f8307c2615512
Rohit Pandey <rohitpandey84205@gmail.com>
2020-07-20 23:35:56 +0530

    Update for loop

--- for_loop.c
+++ for_loop.c
@@ -3,4 +3,8 @@ int main(){
   int i;
   for(i=0;i<10;i++)
     printf("Rohit Pandey");
+  for(i=0;i<10;i++)
+    printf("Sangita Pandey");
+  for(i=0;i<5;i++)
+    printf("Chal chal apne bap ko mat sikha");
 }
